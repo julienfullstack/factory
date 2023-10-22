8f826030c2ec96333d6b0d2380a911142485877a
Julien L <julienlenaz96@gmail.com>
2023-10-21 20:11:15 -0400

    Updated SQL file and program.cs, csproj

--- julien_lenaz.sql
+++ julien_lenaz.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `engineermachines`
+--
+
+DROP TABLE IF EXISTS `engineermachines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `engineermachines` (
+  `EngineerMachineId` int NOT NULL AUTO_INCREMENT,
+  `EngineerId` int NOT NULL,
+  `MachineId` int NOT NULL,
+  PRIMARY KEY (`EngineerMachineId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `engineermachines`
+--
+
+LOCK TABLES `engineermachines` WRITE;
+/*!40000 ALTER TABLE `engineermachines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `engineermachines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `engineers`
 --
 
@@ -37,6 +61,29 @@ LOCK TABLES `engineers` WRITE;
 /*!40000 ALTER TABLE `engineers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `engineers` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `machines`
+--
+
+DROP TABLE IF EXISTS `machines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `machines` (
+  `MachineId` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  PRIMARY KEY (`MachineId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `machines`
+--
+
+LOCK TABLES `machines` WRITE;
+/*!40000 ALTER TABLE `machines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `machines` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -47,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-21 20:03:23
+-- Dump completed on 2023-10-21 20:10:26
