37f241a5b3edc13b679df16251d8b394e5213cdf
Julien L <julienlenaz96@gmail.com>
2023-10-21 20:43:53 -0400

    Fixed bugs

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -14,7 +14,7 @@ namespace Factory
 
       builder.Services.AddControllersWithViews();
 
-      builder.Services.AddDbContext<HairSalonContext>(
+      builder.Services.AddDbContext<FactoryContext>(
                         dbContextOptions => dbContextOptions
                           .UseMySql(
                             builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
